cf1603884963d1cf5bc44362de3928ffc4695578
Bermar Balibalos <bermar@avail.at>
2019-02-13 15:34:44 +0800

    Setup sass

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,14 +10,10 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <script src="{{ mix('/js/app.js') }}"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}" >
 </head>
 <body>
     <div id="app">
