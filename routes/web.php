d101ebf95ba23e611b5d054c0e5763dc0e279f89
Bermar Balibalos <bermar@avail.at>
2019-02-15 18:15:00 +0800

    Add user route middleware

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,34 @@ Route::get('/', 'HomeController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/profile', 'HomeController@index')->name('profile');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/setting', 'HomeController@index')->name('setting');
+  Route::get('/profile', 'ProfileController@index')->name('profile');
+
+  Route::get('/setting', 'HomeController@index')->name('setting');
+
+});
 
 Route::prefix('admin')->group(function () {
+
   Route::get('/', 'Admin\AdminController@index')->name('admin.dashboard');
+
   Route::get('dashboard', 'Admin\AdminController@index')->name('admin.dashboard');
+
   Route::get('register', 'Admin\AdminController@create')->name('admin.register');
+
   Route::post('register', 'Admin\AdminController@store')->name('admin.register.store');
+
   Route::get('login', 'Admin\Auth\LoginController@login')->name('admin.auth.login');
+
   Route::post('login', 'Admin\Auth\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
+
   Route::post('logout', 'Admin\Auth\LoginController@logout')->name('admin.auth.logout');
+
   Route::get('logout', 'Admin\Auth\LoginController@logout')->name('admin.auth.logout');
+
+  Route::middleware(['admin'])->group(function () {
+
+  });
+
 });
